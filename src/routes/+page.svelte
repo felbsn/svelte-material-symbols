cf4437b46ed77df99bf10e40c12cc0143d822570
felbsn <felbsn@gmail.com>
2023-05-10 23:29:08 +0300

    updated package name .c with sorrow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -75,7 +75,7 @@
 
     function copyInstall() {
         copiedInstall = true;
-        navigator.clipboard.writeText(`npm i svelte-material-symbols`);
+        navigator.clipboard.writeText(`npm i @felbsn/svelte-material-symbols`);
 
         startInstallClearCopy();
     }
@@ -164,13 +164,13 @@
         <span style="display: flex;flex-grow: 1;" />
 
         <label class="toggle">
-            <a href="###" style="display: flex;">
+            <a href="https://www.npmjs.com/package/@felbsn/svelte-material-symbols" style="display: flex;">
                 <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
                     ><rect x="227.6" y="213.1" width="28.4" height="57.1" /><path
                         d="M0,156V327.4H142.2V356H256V327.4H512V156ZM142.2,298.9H113.8V213.2H85.3v85.7H28.4V184.6H142.2Zm142.2,0H227.5v28.6H170.6V184.6H284.4Zm199.2,0H455.2V213.2H426.8v85.7H398.4V213.2H370v85.7H313.1V184.6H483.8V298.9Z" /></svg>
             </a>
             <code class="init"
-                >npm i svelte-material-symbols <button
+                >npm i @felbsn/svelte-material-symbols <button
                     class="copy-button"
                     on:click={() => copyInstall()}
                     class:copied-install={copiedInstall}>{copiedInstall ? "copied" : "copy"}</button
@@ -341,7 +341,7 @@
         transition-delay: 1000ms;
     }
     .toggle:hover .init {
-        max-width: 250px;
+        max-width: 280px;
         overflow: hidden;
         padding: 4px;
         transition-delay: 00ms;
--- src/routes/other/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import Symbol from "$lib/Symbol.svelte";
-</script>
-
-<Symbol name="skip_next" />
-
-<a href="/">back</a>
